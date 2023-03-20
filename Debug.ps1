e72f5b9bbe50b97a65ccc35b4d6819fb2b52bc25
Tor Robinson <torrobinson@gmail.com>
2023-03-20 15:29:01 -0600

    Screwing around with rewriting "frames" before actually implementing

--- Debug.ps1
+++ Debug.ps1
@@ -9,4 +9,38 @@ Import-Module -Name "$($PSScriptRoot)\OutConsolePicture.psm1" -Force
 #Get-Help Out-ConsolePicture
 
 # Example usage
-Out-ConsolePicture -Url "https://i.imgur.com/tTmm7sA.png" -Width 16 -Align "Right"
+#Out-ConsolePicture -Url "https://i.imgur.com/tTmm7sA.png" -Width 16 -Align "Right"
+
+
+# TEMPORARY: Animation playground
+$height = 32;
+$firstFrame = 1;
+$lastFrame = 10;
+$frameDelayInMs = 100;
+foreach ( $i in $firstFrame..$lastFrame ) {
+
+
+
+    # Frame 1: draw a frame
+    Out-ConsolePicture -Align "Center" -Width 32 -AlphaThreshold 100 -Url "https://i.imgur.com/Y50DrsD.png" # MARS
+    
+    # Frame 1: Delay
+    Start-Sleep -Milliseconds $frameDelayInMs
+    
+    # Frame 1: Roll back to start of "canvas"
+    [Console]::SetCursorPosition(0, $Host.UI.RawUI.CursorPosition.Y - ($height / 2))
+
+
+
+
+    # Frame 2: draw a frame
+     Out-ConsolePicture -Align "Center" -Width 32                     -Url "https://i.imgur.com/tTmm7sA.png"   # RAINBOW
+    
+    # Frame 2: Delay
+    Start-Sleep -Milliseconds $frameDelayInMs
+    
+    # Frame 2: Roll back to start of "canvas" if it's not the last frame
+    if($i -lt $lastFrame){
+        [Console]::SetCursorPosition(0, $Host.UI.RawUI.CursorPosition.Y - ($height / 2))
+    }
+}
